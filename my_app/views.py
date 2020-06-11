5d0f7b646363ec4cb1b01c56cb8067a3ab0a63a4
dhairyap99 <dhairya.parekh@somaiya.edu>
2020-06-12 01:50:12 +0530

    New_search functionaliy added

--- my_app/views.py
+++ my_app/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    return render(request, 'my_app/new_search.html')
